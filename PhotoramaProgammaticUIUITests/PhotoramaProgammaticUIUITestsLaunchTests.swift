f291c159bde1b88dbbf2618f42e04b2efdeab362
george mapaya <119117609+swiftygeorge@users.noreply.github.com>
2023-02-13 17:56:08 -0500

    Initial Commit

---
+++ PhotoramaProgammaticUIUITests/PhotoramaProgammaticUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoramaProgammaticUIUITestsLaunchTests.swift
+//  PhotoramaProgammaticUIUITests
+//
+//  Created by George Mapaya on 2023-02-13.
+//
+
+import XCTest
+
+final class PhotoramaProgammaticUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
